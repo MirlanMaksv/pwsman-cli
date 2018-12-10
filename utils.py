6068f643d90274cadc9f22ba72c29b2121a531fb
Mirlan <m.maksitaliev@gmail.com>
2018-12-10 06:16:05 +0600

    removed redundant LOC

--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ from base64 import b64decode, b64encode
 
 
 def enc_AES(data, key):
-    data = b64encode(data.encode())
-    key = key
+    # convert to bytes and encode into base64
+    data = data.encode()
     iv = b'iv' + key
     cipher = AES.new(key, AES.MODE_EAX, iv)
     ciphertext = cipher.encrypt(data)
@@ -14,17 +14,18 @@ def enc_AES(data, key):
 
 
 def dec_AES(ciphertext, key):
+    # convert to bytes and decode from base64
     ciphertext = b64decode(ciphertext.encode())
     iv = b'iv' + key
     cipher = AES.new(key, AES.MODE_EAX, iv)
-    data = cipher.encrypt(ciphertext)
-    return b64decode(data).decode()
+    data = cipher.decrypt(ciphertext)
+    return data.decode()
 
 
 def derive_key(password, k_len=32):
     # purposefully prefixed the password
     salt = "salt" + password
-    return scrypt(password, salt, k_len, 16384, 32, 1)
+    return scrypt(password, salt, k_len, 16384, 8, 1)
 
 
 def sha256(psw):
